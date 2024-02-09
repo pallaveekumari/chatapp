786225135ef4a53695e324be24fc1c46154d8a0e
Pallavee <pallaveeratnakar1714@gmail.com>
2024-02-09 23:03:52 +0530

    updation

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import styles from "./Chat.module.css"
 const Chat = () => {
   return (
-    <div className={styles.chat}>
-      <div className={styles.userchat}>
-        <img className={styles.userImg} src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg" alt="" />
-      <div className={styles.userchatInfo}>
-        <span className={styles.userchatName}>Jane</span>
-        <p className={styles.latestMessage}>Hello</p>
-      </div>
-      </div>
-    </div>
+   <div className={styles.chat}>chat</div>
   )
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import styles from "./Sidebar.module.css"
 import Navbar from '../Navbar/Navbar'
 import Searchbar from '../Searchbar/Searchbar'
-import Chat from '../Chat/Chat'
+
+import AllChats from '../AllChats/AllChats'
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       
       <Navbar/>
      <Searchbar/>
-     <Chat/>
+     <AllChats/>
     </div>
   )
 }
